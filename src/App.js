79c5fb3eb575eab34f77682e684b7acd2636d3a8
Bob Hunt <bob.hunt.email@gmail.com>
2019-07-30 13:59:06 -0400

    Created a 'Navigation' component.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Welcome from './components/welcome/Welcome';
 import Howdy from './components/howdy/howdy';
 import Clock from './components/clock/Clock';
 import Contact from './components/contact/Contact.js';
+import Navigation from './components/navigation/Navigation';
 
 function App() {
   return (
     
     <div className="App">
+        <Navigation />
         {/* <Welcome name="Bob" /> */}
         <Route
           exact 
